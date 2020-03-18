f0b6540e0fb7f0098bc64084e86ce20c7175b0f0
Dan Dismuke <dandismuke@gmail.com>
2020-03-17 23:18:30 -0700

    Setting default settings to use published API.

--- src/utils/API.js
+++ src/utils/API.js
@@ -3,18 +3,18 @@ import axios from "axios";
 // Published randomuser API
 const BASEURL = "https://randomuser.me/api/?results=10&nat=us";
 // local database API
-const TEMPURL = 'http://localhost:4000/api/?results=50&nat=us';
+const LOCALDB = 'http://localhost:4000/api/?results=50&nat=us';
 
 export default {
 	getEmployees: function() {
 		// Swap the comments on the next two lines to use the randomuser API
-		//return axios.get(BASEURL)
-		return axios.get(TEMPURL)
+		return axios.get(BASEURL)
+		//return axios.get(LOCALDB)
 	},
 	getEmployeesFilter: function(filter) {
 		// Swap the comments on the next two lines to use the randomuser API
-		//return axios.get(BASEURL +  "&" + filter.filterKey + "=" + filter.filterValue)
-		return axios.get(TEMPURL +  "&" + filter.filterKey + "=" + filter.filterValue)
+		return axios.get(BASEURL +  "&" + filter.filterKey + "=" + filter.filterValue)
+		//return axios.get(LOCALDB +  "&" + filter.filterKey + "=" + filter.filterValue)
 	}
 
 };
